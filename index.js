e762b4d1cca67539ed02f8dfea341793f5f87061
Jackmc <jackmc1122@gmail.com>
2019-07-09 14:09:09 -0700

    pipe content to process.stdout

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const fs = require('fs');
 const readStream = fs.createReadStream('./test.txt');
 const writeStream = fs.createWriteStream('./copy.txt');
 
-readStream.pipe(writeStream);
+readStream
+  .pipe(writeStream);
+
+readStream.pipe(process.stdout);
